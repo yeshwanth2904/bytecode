5a743e49dac76cf682f5964487ac16de445426b5
yeshwanth <yeshwanth@user>
2024-07-03 13:08:48 +0530

    posted

--- projects/src/assingment3/scanner.java
+++ projects/src/assingment3/scanner.java
@@ -4,7 +4,22 @@ public class scanner {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+    Scanner S=new Scanner(System.in);
+    int a=S.nextInt();
+    int b=S.nextInt();
+    int c=S.nextInt();
+    if (a>b && b>c)
+    {
+    	System.out.println("a is greater"+a);
+    }
+    else if(b>c && b>a) 
+       {
+    		System.out.println("b is greater"+b);
+    	}
+    else 
+    {
+    }
+    
 	}
 
 }
